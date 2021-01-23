f0d25672e96c2a4516a6a58a05c7115705647b54
Volodymyr Kyrylenko <kiril2727@gmail.com>
2021-01-23 19:45:03 +0200

    75% done

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import css from './css/styles.css';
+import refs from '../src/refs/refs.js';
+// import pixapi from './js/pixapi.js';
+
+import templateItems from './templates/items.hbs';
+// console.log(templateItems);
+
+import pixapi from './js/pixapi.js';
+// import oneCountryInfo from './templates/one-country.hbs';
+// // імпорт з бібліотеки "lodash" підхіду "debounce"
+// import debounce from 'lodash/debounce';
+// // імпорт нотифікації , щоб необхідно зробити запит більш специфічним (і тем під неї)
+// import { error } from '@pnotify/core/';
+// import '@pnotify/core/dist/BrightTheme.css';
+// import '@pnotify/core/dist/PNotify.css';
---
+++ src/refs/refs.js
@@ -0,0 +1,7 @@
+export default {
+  input: document.querySelector('.search-input'),
+  form: document.querySelector('.search-form'),
+  searchBtn: document.querySelector('.search-button'),
+  list: document.querySelector('.gallery'),
+  moreBtn: document.querySelector('.more'),
+};
